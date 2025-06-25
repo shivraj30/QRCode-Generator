f7b73908f3adb562d6ff6a1120518a8ac1cc473f
shivraj30 <shivrajdevadiga218@gmail.com>
2025-06-25 09:20:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import io, base64
+from qrcode import QRCode
+from flask import Flask, render_template, request, redirect, url_for
+
+
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        qr = QRCode()
+        data = request.form['data']
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image()
+        buffer = io.BytesIO()
+        img.save(buffer, format='PNG')
+        img_str = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        return redirect(url_for('qrcode', img_str=img_str))
+
+
+    return render_template('index.html')
+
+@app.route('/qrcode')
+def qrcode():
+    img_str = request.args.get('img_str')
+    return render_template('qrcode.html', img_str=img_str)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
